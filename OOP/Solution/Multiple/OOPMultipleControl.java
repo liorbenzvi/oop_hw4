3be519ccf6013e9b606e8301d3563d3e79dce5dc
liorbenzvi <liorbenzvi1907@gmail.com>
2017-05-27 10:46:43 +0300

    added files

---
+++ OOP/Solution/Multiple/OOPMultipleControl.java
@@ -0,0 +1,38 @@
+package OOP.Solution.Multiple;
+
+import OOP.Provided.Multiple.OOPMultipleException;
+import java.io.File;
+
+public class OOPMultipleControl {
+
+    //TODO: DO NOT CHANGE !!!!!!
+    private Class<?> interfaceClass;
+    private File sourceFile;
+
+    //TODO: DO NOT CHANGE !!!!!!
+    public OOPMultipleControl(Class<?> interfaceClass, File sourceFile) {
+        this.interfaceClass = interfaceClass;
+        this.sourceFile = sourceFile;
+    }
+
+    //TODO: fill in here :
+    public void validateInheritanceGraph() throws OOPMultipleException {
+
+    }
+
+    //TODO: fill in here :
+    public Object invoke(String methodName, Object[] args)
+            throws OOPMultipleException {
+        return null;
+    }
+
+    //TODO: add more of your code :
+
+
+    //TODO: DO NOT CHANGE !!!!!!
+    public void removeSourceFile() {
+        if (sourceFile.exists()) {
+            sourceFile.delete();
+        }
+    }
+}
