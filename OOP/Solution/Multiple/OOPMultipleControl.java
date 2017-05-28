bbb4774edad5670d582c1652e2fbfe8109dff9ac
Eden Ben David <edenbd@gmail.com>
2017-05-28 23:17:20 +0300

    done my a part

--- OOP/Solution/Multiple/OOPMultipleControl.java
+++ OOP/Solution/Multiple/OOPMultipleControl.java
@@ -1,14 +1,15 @@
 package OOP.Solution.Multiple;
 
+import OOP.Provided.Multiple.OOPCoincidentalAmbiguity;
 import OOP.Provided.Multiple.OOPInherentAmbiguity;
 import OOP.Provided.Multiple.OOPMultipleException;
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+
 import OOP.Provided.Multiple.OOPBadClass;
+import javafx.util.Pair;
 
 public class OOPMultipleControl {
 
@@ -54,9 +55,40 @@ public class OOPMultipleControl {
     }
 
     //TODO: fill in here :
-    public Object invoke(String methodName, Object[] args)
-            throws OOPMultipleException {
-        return null;
+    public Object invoke(String methodName, Object[] args) throws OOPMultipleException {
+        List<Class> interfaces = Arrays.asList(interfaceClass.getInterfaces());
+        HashSet<Class> visited = new HashSet<>();
+        List<Class> newInterfaces = new ArrayList<>();
+        ArrayList<Pair<Class<?>, Method>> candidates = new ArrayList<>();
+        Class[] cArgs = Arrays.stream(args).map(arg -> arg.getClass()).toArray(Class[]::new);;
+
+        while (!interfaces.isEmpty()){
+            for (Class klass : interfaces){
+                try {
+                    Method invokedMethod = klass.getDeclaredMethod(methodName, cArgs);
+                    Pair p = new Pair(klass,invokedMethod);
+                    candidates.add(p);
+                }
+                catch(NoSuchMethodException e){}
+                newInterfaces.addAll(Arrays.asList(klass.getInterfaces()));
+                visited.add(klass);
+            }
+            interfaces.clear();
+            interfaces.addAll(newInterfaces);
+            newInterfaces.clear();
+        }
+        if (candidates.size() > 1){
+            throw new OOP.Provided.Multiple.OOPCoincidentalAmbiguity(candidates);
+        }
+        Object ret = null;
+        Pair p = candidates.get(0);
+        Method methodToInvoke = (Method)p.getValue();
+        Class classToInvoke = (Class)p.getKey();
+        try{
+            ret = methodToInvoke.invoke(classToInvoke,args);
+        }catch (Exception e){ throw new OOPCoincidentalAmbiguity(candidates);}
+
+        return ret;
     }
 
     //TODO: add more of your code :
