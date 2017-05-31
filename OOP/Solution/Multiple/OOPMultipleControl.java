01d95d974e0c6349b042ba83ff6e5fa9bd570b6b
Eden Ben David <edenbd@gmail.com>
2017-05-31 12:25:38 +0300

    test part a passing

--- OOP/Solution/Multiple/OOPMultipleControl.java
+++ OOP/Solution/Multiple/OOPMultipleControl.java
@@ -26,9 +26,9 @@ public class OOPMultipleControl {
     //TODO: fill in here :
     public void validateInheritanceGraph() throws OOPMultipleException {
 
-        List<Class> interfaces = Arrays.asList(interfaceClass.getInterfaces());
+        List<Class> interfaces = new LinkedList<Class>(Arrays.asList(interfaceClass.getInterfaces()));
         HashSet<Class> visited = new HashSet<>();
-        List<Class> newInterfaces = new ArrayList<>();
+        List<Class> newInterfaces = new LinkedList<>();
 
         while (!interfaces.isEmpty()){
             for (Class i : interfaces) {
@@ -54,30 +54,46 @@ public class OOPMultipleControl {
 
     }
 
+    Pair<Class<?>, Method> checkInInterfaceClass(Class in, String methodName, Class[] argsClass)
+            throws NoSuchMethodException{
+        String inName = in.getName();
+        StringBuilder klassName = new StringBuilder(inName);
+        klassName.setCharAt(inName.length()-2, 'C');
+        Method me;
+        Class klass;
+        try{
+            klass = Class.forName(klassName.toString());
+        }catch(Exception e){ throw new NoSuchMethodException();}
+        try{
+            me = klass.getMethod(methodName,argsClass);
+        } catch(Exception e){ throw new NoSuchMethodException();}
+        return new Pair(klass,me);
+    }
+
     //TODO: fill in here :
     public Object invoke(String methodName, Object[] args) throws OOPMultipleException {
-        List<Class> interfaces = Arrays.asList(interfaceClass.getInterfaces());
+        List<Class> interfaces = new LinkedList<Class>(Arrays.asList(interfaceClass.getInterfaces()));
         HashSet<Class> visited = new HashSet<>();
-        List<Class> newInterfaces = new ArrayList<>();
+        List<Class> newInterfaces = new LinkedList<>();
         ArrayList<Pair<Class<?>, Method>> candidates = new ArrayList<>();
+        if(args == null) args = new Object[0];
         Class[] cArgs = Arrays.stream(args).map(arg -> arg.getClass()).toArray(Class[]::new);;
 
         while (!interfaces.isEmpty()){
-            for (Class klass : interfaces){
+            for (Class in : interfaces){
                 try {
-                    Method invokedMethod = klass.getDeclaredMethod(methodName, cArgs);
-                    Pair p = new Pair(klass,invokedMethod);
+                    Pair p = checkInInterfaceClass(in,methodName,cArgs);
                     candidates.add(p);
                 }
                 catch(NoSuchMethodException e){}
-                newInterfaces.addAll(Arrays.asList(klass.getInterfaces()));
-                visited.add(klass);
+                newInterfaces.addAll(Arrays.asList(in.getInterfaces()));
+                visited.add(in);
             }
             interfaces.clear();
             interfaces.addAll(newInterfaces);
             newInterfaces.clear();
         }
-        if (candidates.size() > 1){
+        if (candidates.size() > 1 || candidates.size() == 0){
             throw new OOP.Provided.Multiple.OOPCoincidentalAmbiguity(candidates);
         }
         Object ret = null;
@@ -85,9 +101,8 @@ public class OOPMultipleControl {
         Method methodToInvoke = (Method)p.getValue();
         Class classToInvoke = (Class)p.getKey();
         try{
-            ret = methodToInvoke.invoke(classToInvoke,args);
+            ret = methodToInvoke.invoke(classToInvoke.newInstance(),args);
         }catch (Exception e){ throw new OOPCoincidentalAmbiguity(candidates);}
-
         return ret;
     }
 
